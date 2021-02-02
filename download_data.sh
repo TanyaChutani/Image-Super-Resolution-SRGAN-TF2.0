3beb61e5edba79e444dc84d420880aec40a90b59
Tanya <tanyach1997@gmail.com>
2021-02-02 15:47:44 +0530

    added script for downloading data

---
+++ download_data.sh
@@ -0,0 +1,5 @@
+wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_HR.zip
+wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_HR.zip
+
+unzip DIV2K_train_HR.zip
+unzip DIV2K_valid_HR.zip
