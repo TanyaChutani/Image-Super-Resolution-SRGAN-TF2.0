5d1fff7c8c5b1ac5aee069f9f5d02ea8538cc3d0
Tanya <tanyach1997@gmail.com>
2021-03-01 01:22:18 +0530

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -1,5 +1,5 @@
-wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_HR.zip
-wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_HR.zip
+curl -O http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_HR.zip
+curl -O http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_HR.zip
 
 unzip DIV2K_train_HR.zip
 unzip DIV2K_valid_HR.zip
